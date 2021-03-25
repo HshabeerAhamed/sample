1d2134a5b28cfc71a550b52d422e3c2b29a40862
Hshabeer123 <HshabeerAhamed>
2021-03-25 15:25:11 +0530

    New

---
+++ src/com/gitexample/GitSample.java
@@ -0,0 +1,14 @@
+package com.gitexample;
+
+public class GitSample {
+
+	public static void main(String[] args) 
+	{
+         int a = 10;
+         int b = 20;
+         
+         System.out.println(a);
+         System.out.println(b);
+	}
+
+}
